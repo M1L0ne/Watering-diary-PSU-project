70481a5b976625e9c565b545890f85626a90510d
M1L0ne <m1l0ne@mail.ru>
2026-01-05 21:38:36 +0500

    Перешёл на jdk 21

---
+++ src/main/java/tsygvintsev/watering_diary/WateringDiaryApplication.java
@@ -0,0 +1,13 @@
+package tsygvintsev.watering_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WateringDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tsygvintsev/watering_diary/WateringDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package tsygvintsev.watering_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WateringDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
